5b475772ba30e096068a9b4b3d208eab0810862b
BaianorASR <reis.adson2@outlook.com>
2022-02-10 20:28:44 -0300

    primeiro backup

---
+++ src/img/url.js
@@ -0,0 +1,60 @@
+
+const URLS = {
+  barbarian: {
+    1: 'https://i.pinimg.com/originals/3f/03/0e/3f030e31536820ac34b3463ffae65e87.gif',
+    2: 'https://i.pinimg.com/originals/4b/86/c7/4b86c75cde91f31ec79ea2bfb1bef0f8.gif',
+    3:'https://i.pinimg.com/originals/15/de/fc/15defcb5f35239554da784918902b32a.gif',
+  },
+  cleric: {
+    1: 'https://i.imgur.com/llNCpQF.gif',
+    2: 'https://cdn.pixilart.com/photos/large/87f9d6104eadfc4.gif',
+    3: 'https://i.pinimg.com/originals/04/96/7c/04967c814e98570fbffe8329fe36d2bc.gif',
+  },
+  druid: {
+    1: 'https://i.pinimg.com/originals/8e/6a/dc/8e6adc9d7b5d9e47c29cfdf2b7313e27.gif',
+    2: 'https://i.pinimg.com/originals/eb/6d/67/eb6d6784b2a6382c5502814adc4ca6d8.gif',
+    3: 'https://i.pinimg.com/originals/bf/22/47/bf2247e2b02cdf7c2ff573c6629895a6.gif',
+  },
+  fighter: {
+    1: 'https://i.pinimg.com/originals/2a/30/05/2a3005e5e72f979c470d6ab695f31166.gif',
+    2: 'https://i.pinimg.com/originals/ff/06/2b/ff062be5f994a33f4d3df605188797d2.gif',
+    3: 'https://i.pinimg.com/originals/19/7b/88/197b887956c1741536cbda7a8bf0c59c.gif',
+  },
+  monk: {
+    1: 'https://i.pinimg.com/originals/bf/22/47/bf2247e2b02cdf7c2ff573c6629895a6.gif',
+    2: 'https://i.pinimg.com/originals/b4/c9/4a/b4c94a3630dd4375639d508c195147f8.gif',
+    3: 'https://i.imgur.com/UjD7THw.gif',
+  },
+  paladin: {
+    1: 'https://i.pinimg.com/originals/f0/6a/a4/f06aa45318cce9f16f2b3e591a138ae1.gif',
+    2: 'https://i.pinimg.com/originals/15/de/fc/15defcb5f35239554da784918902b32a.gif',
+    3: 'https://64.media.tumblr.com/6d0b0dab560d34273d69b775fc19ccf9/tumblr_o3h6baD1Tg1u77u56o1_540.gifv',
+  },
+  ranger: {
+    1: 'https://gifimage.net/wp-content/uploads/2017/10/duelyst-gif-11.gif',
+    2: 'https://64.media.tumblr.com/0639fb88fce70bd04f4edd84ced274eb/tumblr_o1bu7hxReb1rznluto1_r1_250.gifv',
+    3: 'https://i.pinimg.com/originals/8c/d8/9c/8cd89c36fdb3215e7b7f82a8e94605d2.gif',
+  },
+  rogue: {
+    1: 'https://i.pinimg.com/originals/74/85/89/7485898c0af42a6f9b66cdbb8978ebce.gif',
+    2: 'https://i.pinimg.com/originals/b7/90/48/b79048c9ee831cccdd38c34099722a75.gif',
+    3: 'https://i.pinimg.com/originals/9e/0a/08/9e0a0822f4f91e591cf3811b5357c3a1.gif',
+  },
+  sorcerer: {
+    1: 'https://i.pinimg.com/originals/4c/ac/da/4cacdafc7e11b9162c3dcec230bca642.gif',
+    2: 'https://i.pinimg.com/originals/12/e9/aa/12e9aa3d985b6001ebe1174256cf48b8.gif',
+    3: 'https://i.pinimg.com/originals/f0/6a/a4/f06aa45318cce9f16f2b3e591a138ae1.gif',
+  },
+  warlock: {
+    1: 'https://3.bp.blogspot.com/-Eeqo6A3dIrg/WN_jN7fmzzI/AAAAAAAAFQg/Y0cfXJ_ZI7M87c4kST8gP_hLe65ZolcEwCLcB/s1600/Phantasm_attack_8.gif',
+    2: 'https://i.pinimg.com/originals/40/4f/d9/404fd93484c2592c78a13cf25891c156.gif',
+    3: 'https://i.pinimg.com/originals/68/d9/56/68d956087ac7352b07b9295d72849a22.gif',
+  },
+  wizard: {
+    1: 'https://i.pinimg.com/originals/0e/31/b8/0e31b8daeb1920eb4b852a8628710223.gif1',
+    2: 'https://i.pinimg.com/originals/b6/40/30/b64030bf5b203da3e619dedfa0180f33.gif',
+    3: 'https://i.imgur.com/WmYiLCy.gif',
+  },
+}
+
+export default URLS;
